d8bec02a9bde400f4ddd2e737e63fa6bcdf2175a
Tanvi Trivedi <tanvvitrivedi27@gmail.com>
2024-01-23 16:19:08 +0530

    Created API for CRUD

---
+++ Models/MyDBContext.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace ASPCoreWebAPICRUD.Models
+{
+    public partial class MyDBContext : DbContext
+    {
+        public MyDBContext()
+        {
+        }
+
+        public MyDBContext(DbContextOptions<MyDBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Student> Students { get; set; } = null!;
+        public virtual DbSet<UserTbl> UserTbls { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Student>(entity =>
+            {
+                entity.Property(e => e.FatherName)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.StudentGender)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.StudentName)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<UserTbl>(entity =>
+            {
+                entity.ToTable("user_tbl");
+
+                entity.Property(e => e.Email)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Gender)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Name)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Password)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
